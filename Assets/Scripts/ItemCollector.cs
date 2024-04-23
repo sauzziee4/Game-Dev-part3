e662c0fd08487dd088fec647ae86db80a3e70f97
Max Schulte <maxgs1001@gmail.com>
2024-04-23 12:50:21 +0200

    files

---
+++ Assets/Scripts/ItemCollector.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+public class ItemCollector : MonoBehaviour
+{
+    
+    
+    public static float collectibles = 0;
+    
+    private void OnTriggerEnter(Collider other)
+    {
+
+        //when the player collects a collectible the counter goes up by one
+        if (other.gameObject.CompareTag("Collectable"))
+        {
+            Destroy(other.gameObject);
+            collectibles++;
+            
+            
+            
+        }
+    }
+}
