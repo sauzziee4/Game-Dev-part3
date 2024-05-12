4be2c50b88c7b319b78e4803c34ad38a851d5bf7
Max Schulte <maxgs1001@gmail.com>
2024-05-12 14:51:41 +0200

    Pickup manager is ready

--- Assets/Scripts/Part 2 Scripts/PickUps/PickUpManager.cs
+++ Assets/Scripts/Part 2 Scripts/PickUps/PickUpManager.cs
@@ -8,8 +8,12 @@ public class PickUpManager : MonoBehaviour
     public static bool checkPower { get; set; }
     public float duration = 5f;
     // public PowerUpEffects powerUpEffect;
+
+    //pickup 2
     public static bool pk2;
+    //pickup 3
     public static bool pk3;
+    //pickup4
     public static bool pk4;
 
 
@@ -27,14 +31,30 @@ public class PickUpManager : MonoBehaviour
         if(pk2==true)
         {
             Debug.Log("picup2 two has been hit");
+            StartCoroutine(Pickup2());
 
         }
         if (pk3 == true)
         {
             Debug.Log("pickup3 has been hit");
+            StartCoroutine (Pickup3());
 
         }
     }
+    public IEnumerator Pickup2()
+    {
+        //the code for the second pickup will go in here
+        yield return new WaitForSeconds(1f);
+    }
+    public IEnumerator Pickup3()
+    {
+        //the code for the third pickup woill go in here
+        yield return new WaitForSeconds(1f);
+    }
+    
+
+
+    //This is old code from when Muhmmed uploaded his pickup scripts
     public IEnumerator SpeedRoutine()
     {
         GameObject player = GameObject.FindGameObjectWithTag("Player").gameObject;
